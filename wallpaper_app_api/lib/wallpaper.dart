caff23c8eb3562592d114c901245d0328aecde86
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-08-08 18:52:29 +0530

    Show Images and Load More

--- wallpaper_app_api/lib/wallpaper.dart
+++ wallpaper_app_api/lib/wallpaper.dart
@@ -11,12 +11,14 @@ class Wallpaper extends StatefulWidget {
 }
 
 class _WallpaperState extends State<Wallpaper> {
-List images = [];
-    @override
-  void initState(){
+  List images = [];
+  int page = 1;
+  @override
+  void initState() {
     super.initState();
     fetchtheapi();
   }
+
   fetchtheapi() async {
     await https.get(Uri.parse("https://api.pexels.com/v1/curated?per_page=80"),
         headers: {
@@ -28,10 +30,26 @@ List images = [];
         setState(() {
           images = result['photos'];
         });
-        print(images);
       },
     );
   }
+
+  loadmore() async {
+    setState(() {
+      page += 1;
+    });
+    String url = 'https://api.pexels.com/v1/curated?per_page=80&page=$page';
+    await https.get(Uri.parse(url), headers: {
+      'Authorization':
+          'TyVyqPegTtrdJRA3yoUbHopZyhdd1gPF1HTcPbrME0tM2XGhqevWvJw0'
+    }).then((value) {
+      Map result = jsonDecode(value.body);
+      setState(() {
+        images.addAll(result['photos']);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,48 +60,44 @@ List images = [];
         children: [
           Expanded(
             child: Container(
+              padding: const EdgeInsets.all(9),
               child: GridView.builder(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 3,
                   childAspectRatio: 1 / 2,
-                  crossAxisSpacing: 5,
-                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 7,
+                  mainAxisSpacing: 7,
                 ),
-                itemBuilder: (ctx, index) => InkWell(
+                itemBuilder: (ctx, i) => InkWell(
                   onTap: () {},
                   child: Container(
                     clipBehavior: Clip.hardEdge,
                     decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                          colors: [
-                            Color.fromARGB(255, 255, 0, 0),
-                            Color.fromARGB(255, 255, 125, 0),
-                            Color.fromARGB(255, 0, 255, 0),
-                            Color.fromARGB(255, 0, 255, 125),
-                            Color.fromARGB(255, 0, 0, 255),
-                            Color.fromARGB(255, 0, 125, 255),
-                          ],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter),
                       borderRadius: BorderRadius.circular(16),
                     ),
-                    child: Image.network('src'),
+                    child: Image.network(
+                      images[i]['src']['tiny'],
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-                itemCount: 100000,
+                itemCount: images.length,
               ),
             ),
           ),
-          Container(
-            height: 60,
-            width: double.infinity,
-            color: Colors.white,
-            child: const Center(
-              child: Text(
-                'Load More..',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
+          InkWell(
+            onTap: () => loadmore(),
+            child: Container(
+              height: 60,
+              width: double.infinity,
+              color: Colors.black,
+              child: const Center(
+                child: Text(
+                  'Load More..',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),

f4b411be2fd01d626c6bdd7666743bf9d2ee3e56
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-08-08 19:47:16 +0530

    Wallpaper Set

--- wallpaper_app_api/lib/fullscreen.dart
+++ wallpaper_app_api/lib/fullscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
 
 class Fullscreen extends StatefulWidget {
   const Fullscreen({super.key, required this.imageUrl});
@@ -10,16 +12,43 @@ class Fullscreen extends StatefulWidget {
 }
 
 class _FullscreenState extends State<Fullscreen> {
+  Future<void> setWallpaper(BuildContext context) async {
+    int location = WallpaperManager.BOTH_SCREEN;
+    var file = await DefaultCacheManager().getSingleFile(widget.imageUrl);
+    bool result =
+        await WallpaperManager.setWallpaperFromFile(file.path, location);
+    if (result) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Wallpaper set successfully!')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to set wallpaper.')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
       body: Column(
         children: [
           Expanded(
             child: Image.network(widget.imageUrl),
           ),
           InkWell(
-            onTap: () {},
+            onTap: () => setWallpaper(context),
             child: Container(
               height: 60,
               width: double.infinity,
